214d867a34f8303ad0f7184b2aff84e5a1dbe9e0
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2024-12-24 00:45:36 +0500

    an update

--- controllers/synthflowController.js
+++ controllers/synthflowController.js
@@ -715,7 +715,7 @@ async function initiateCall(
   test = false
 ) {
   console.log("IS call test ", test);
-  console.log("Call data is ", data);
+  // console.log("Call data is ", data);
   try {
     let synthKey = process.env.SynthFlowApiKey;
 
