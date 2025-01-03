392aca4a758ff1c2c11e8a8076bc6eaf41b4c27b
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2025-01-04 01:57:54 +0500

    default timezone for agents provided

--- constants/constants.js
+++ constants/constants.js
@@ -2,4 +2,5 @@ export const constants = {
   RedeemCodeSeconds: 30 * 60, // Seconds
   GiftDontCancelPlanSeconds: 30 * 60, // Seconds
   MinThresholdSeconds: 2 * 60,
+  DefaultTimezone: "America/Los_Angeles",
 };
