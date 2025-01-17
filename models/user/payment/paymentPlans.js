8cc981058a65e4af953d24c05fdf1127e8bead40
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2025-01-17 21:44:12 +0500

    reduced plan prices more

--- models/user/payment/paymentPlans.js
+++ models/user/payment/paymentPlans.js
@@ -77,17 +77,17 @@ export const PayAsYouGoPlans = [
   { type: PayAsYouGoPlanTypes.Plan30Min, price: 0.15, duration: 30 * 60 },
   {
     type: PayAsYouGoPlanTypes.Plan120Min,
-    price: 0.3,
+    price: 0.2,
     duration: 120 * 60,
   },
   {
     type: PayAsYouGoPlanTypes.Plan360Min,
-    price: 0.45,
+    price: 0.25,
     duration: 360 * 60,
   },
   {
     type: PayAsYouGoPlanTypes.Plan720Min,
-    price: 0.6,
+    price: 0.3,
     duration: 720 * 60,
   },
 ];
