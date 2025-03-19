2e3e33cdf09fc9ea8c0dcafa972023b63ac1f51d
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2025-03-20 03:11:53 +0500

    an update

--- resources/LeadResource.js
+++ resources/LeadResource.js
@@ -209,7 +209,7 @@ async function getUserData(lead, currentUser = null) {
 
       const code = dncData.RESPONSECODE;
 
-      if (code === "OK") {
+      if (code === "Ok") {
         leadData.codeOk = true;
         isOnDncList =
           dncData.national_dnc === "Y" ||
