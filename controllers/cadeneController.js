0a8056b14d27528a089e4c188eff57279a249d64
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2025-04-15 20:33:00 +0500

    an udate

--- controllers/cadeneController.js
+++ controllers/cadeneController.js
@@ -585,7 +585,9 @@ export const CronRunCadenceCallsFirstBatch = async () => {
         // WriteToFile("Next call to be sent is ", calls.length + 1);
         let lastCall = calls[calls.length - 1];
         let nextCadenceCall = callCadence[calls.length];
-        console.log("nextCallCadence ", nextCadenceCall);
+        console.log("All cadence ", callCadence);
+
+        console.log(`nextCallCadence ${calls.length}`, nextCadenceCall);
         let waitTime =
           Number(nextCadenceCall.waitTimeDays) * 24 * 60 +
           Number(nextCadenceCall.waitTimeHours) * 60 +
