7420569822db0c7b01f3d56024846a77a091d3ec
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2025-04-16 19:37:57 +0500

    agency xbar plans list and create apis

--- controllers/agency/AgencyPaymentPlansController.js
+++ controllers/agency/AgencyPaymentPlansController.js
@@ -135,6 +135,96 @@ export async function GetAgencyHostedPlans(req, res) {
   });
 }
 
+export async function CreateAgencyHostedXbarPlan(req, res) {
+  JWT.verify(req.token, process.env.SecretJwtKey, async (error, authData) => {
+    if (authData) {
+      let userId = authData.user.id;
+      let {
+        originalPrice,
+        discountedPrice = null,
+        percentageDiscount = null,
+        minutes,
+        title,
+        planDescription,
+        tag = null,
+      } = req.body;
+
+      console.log("Update User ", userId);
+      // console.log("Update data ", req);
+      let user = await db.User.findByPk(userId);
+      console.log("Connected Account ", user.connectedAccountId);
+
+      if (user.userRole == UserRole.Agency) {
+        let plan = await db.AgencyHostedXbarPlans.create({
+          type: `Plan${minutes}`,
+          title: title,
+          planDescription: planDescription,
+
+          originalPrice: originalPrice,
+          discountedPrice: discountedPrice,
+          percentageDiscount: percentageDiscount,
+          tag: tag,
+          userId: user.id,
+        });
+
+        return res.send({
+          status: true,
+          data: plan,
+          message: "Plan created",
+        });
+      } else {
+        return res.send({
+          status: false,
+          data: null,
+          message: "Only available for agency accounts",
+        });
+      }
+    } else {
+      return res.send({
+        status: false,
+        data: null,
+        message: "Unauthenticated user",
+      });
+    }
+  });
+}
+
+export async function GetAgencyHostedXbarPlans(req, res) {
+  JWT.verify(req.token, process.env.SecretJwtKey, async (error, authData) => {
+    if (authData) {
+      let userId = authData.user.id;
+
+      let user = await db.User.findByPk(userId);
+
+      if (user.userRole == UserRole.Agency) {
+        let plans = await db.AgencyHostedXbarPlans.findAll({
+          where: {
+            userId: user.id,
+          },
+        });
+
+        return res.send({
+          status: true,
+          data: plans,
+          message: "Xbar Plans obtained",
+        });
+      } else {
+        return res.send({
+          status: false,
+          data: null,
+          message: "Only available for agency accounts",
+        });
+      }
+    } else {
+      return res.send({
+        status: false,
+        data: null,
+        message: "Unauthenticated user",
+      });
+    }
+  });
+}
+
 export async function SubscribePlan(req, res) {
   JWT.verify(req.token, process.env.SecretJwtKey, async (error, authData) => {
     if (authData) {
